4caac3d310723b9a3f40399146fbb44d0199426a
Vadim Harbar <74145301+VHarbar@users.noreply.github.com>
2021-04-09 16:29:33 +0300

    Add files via upload

---
+++ lab08/prj/TestDriver/ModuleHarbar.h
@@ -0,0 +1,64 @@
+#ifndef MODULESHARBAR_H
+#define MODULESHARBAR_H
+#include <iostream>
+#include<math.h>
+
+using namespace std;
+
+float check(float num){
+    bool valid = false;
+    while (!valid){
+        valid = true;
+        cout << "Введiть рацiональне число: ";
+        cin >> num;
+        if(cin.fail()) {
+            cin.clear();
+            cin.ignore();
+            valid = false;
+        }
+
+    }
+    return num;
+}
+string Name_of_Developer()
+{
+    string name;
+    name="Версiя 1.0\nРозробник: Гарбар Вадим Володимирович.\nЦНТУ, 2021 рiк.<c>";
+    return name;
+}
+bool result(){
+    char a=0;
+    char b=0;
+    cout <<"a: ";
+    cin >> a;
+    cout<<"b: ";
+    cin>>b;
+    if(a+10>=b){
+        return true;
+    }
+    else return false;
+}
+double S_calculation(){
+    double S=0,x=0,y=0,z=0;
+   double PI = 3.141592653589793;
+   cout<<"\nX - ";
+      x=check(x);
+      while(x<0|| x==0){
+          if(x<0 || x==0){
+              cout<<"Введiть число б?льше 0";
+          }
+          x=check(x);
+      }
+      while(x<y || y==0){
+      cout<<"Y - ";
+      if(x<y){
+        cout<<"Y має бути менше нiж x";
+      }
+      y=check(y);
+      }
+      cout<<"Z - ";
+      z=check(z);
+    S=log(x-y)+sqrt(PI*pow(x,2)/x+z/2*pow(y,2));
+    return S;
+}
+#endif // MODULESHARBAR_H
